34efce9cc7f32049d1d9b4fb320aa3dc16922699
ninaprimer <ninaprimer@gmail.com>
2020-07-13 11:52:18 +0100

    adding files

---
+++ build/precache-manifest.d1ca92f218294348e419ddaaf4bc50f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4563d7a215c60802a0b854288170d5e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "75c6489054202c471dd7",
+    "url": "/static/css/main.6f99bb44.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c6489054202c471dd7",
+    "url": "/static/js/main.6cfd0b6f.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
